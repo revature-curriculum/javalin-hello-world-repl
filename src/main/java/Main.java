22b3894dd2f9b5339f1a99a09abc1eec76397e76
nballar <69126984+nballar@users.noreply.github.com>
2022-02-08 16:08:52 +0000

    updated answer again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(4100);
 
-        app.get("/", ctx -> ctx.result("Hello, I'm Ashoka, and I built a web application"));
+        app.get("/", ctx -> ctx.result("Hello, I'm Ashoka, and I built a web application!"));
 
     }
 }
